42185970c4d3a75974a1cdd429e3d5c3574506d5
Andrew Shu <talklittle@gmail.com>
2017-10-29 17:18:34 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mastodon_dl',
+      version='0.1.0',
+      author='Andrew Shu',
+      author_email='talklittle@gmail.com',
+      description='Download media from Mastodon Social',
+      install_requires=['Mastodon.py >= 1.1.1'])
