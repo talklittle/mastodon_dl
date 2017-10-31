6e66d2f1472a4033b80cc255d6b0decc78817f0c
Andrew Shu <talklittle@gmail.com>
2017-10-30 21:04:39 -0700

    download: get media URLs and download

--- mastodon_dl.py
+++ mastodon_dl.py
@@ -2,6 +2,8 @@
 
 import argparse
 from mastodon import Mastodon
+import os
+import requests
 
 
 def main():
@@ -18,11 +20,20 @@ def main():
         since_id=args.since_id,
         limit=args.limit
     )
-    print(timeline)
+
+    page = timeline
+    while page is not None:
+        process_page(page, args.output_dir)
+        page = mastodon.fetch_next(timeline)
+
+    print('Done!')
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Download media from a Mastodon feed.')
+    parser.add_argument('-o', '--output-dir',
+                        default=os.path.join(os.curdir, 'downloads'),
+                        help='Directory to save files')
     parser.add_argument('-u', '--email', help='Email address for login')
     parser.add_argument('-p', '--password', help='Password for login')
     parser.add_argument('-s', '--secret-file', help='Path to API client secret file')
@@ -47,6 +58,46 @@ def init_mastodon(email, password, client_secret, api_base_url):
         password=password
     )
     return mastodon
+
+
+def process_page(page, output_dir):
+    for toot in page:
+        media_attachments = toot['media_attachments']
+        if media_attachments is not None:
+            for media_attachment in media_attachments:
+                url = media_attachment['url']
+                remote_url = media_attachment['remote_url']
+
+                if remote_url is not None:
+                    out_filename = os.path.basename(remote_url)
+                else:
+                    out_filename = os.path.basename(url)
+
+                os.makedirs(output_dir, exist_ok=True)
+
+                out_path = os.path.join(output_dir, out_filename)
+                if os.path.exists(out_path):
+                    print("Skipping %s -- %s exists" % (url, out_path))
+                else:
+                    stream_to_file(url, out_path)
+
+
+def stream_to_file(url, out_path):
+    """
+    Download to a file.
+    http://docs.python-requests.org/en/master/user/quickstart/#raw-response-content
+    :param url: URL to download
+    :param out_path: Path to output file.
+    """
+    with requests.get(url, stream=True) as r:
+        try:
+            with open(out_path, 'wb') as fd:
+                for chunk in r.iter_content(chunk_size=128):
+                    fd.write(chunk)
+            print("Downloaded %s to %s" % (url, out_path))
+        except KeyboardInterrupt:
+            print("Error downloading %s to %s" % (url, out_path))
+            os.remove(out_path)
 
 
 if __name__ == '__main__':
